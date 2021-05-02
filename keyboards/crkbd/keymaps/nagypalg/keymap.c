74ff51451199d7a2254845346235cb6a915b2e3d
Gabor Nagypal <nagypalg@gmail.com>
2021-05-02 18:24:44 +0200

    somewhat working colemak 5 layer keymap

--- keyboards/crkbd/keymaps/nagypalg/keymap.c
+++ keyboards/crkbd/keymaps/nagypalg/keymap.c
@@ -18,15 +18,26 @@ extern uint8_t is_master;
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
 enum layers {
-  _QWERTY,
-  _LOWER,
-  _RAISE,
-  _ADJUST
+  _BASE,
+  _NUM,
+  _SYM,
+  _NAV,
+  _FUNC,
+  _MOUSE
 };
 
-// Custom keycodes for layer keys
-// Dual function escape with left command
-#define KC_LGESC LGUI_T(KC_ESC)
+// Custom keycodes
+
+#define FUNC_TAB_CKC LT(_FUNC,KC_TAB)
+#define NUM_CKC MO(_NUM)
+#define SYM_CKC MO(_SYM)
+#define NAV_CKC MO(_NAV)
+#define MOUSE_CKC TO(_MOUSE)
+#define BASE_CKC TO(_BASE)
+
+#define C_S_V LCTL(LSFT(KC_V))
+#define C_S_F LCTL(LSFT(KC_F))
+
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
@@ -38,65 +49,24 @@ enum custom_keycodes {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [_QWERTY] = LAYOUT(
-  //,-----------------------------------------.                ,---------------------------------------------.
-     KC_TAB,  KC_Q,  KC_W,  KC_F,  KC_P,  KC_B,                   KC_J,  KC_L,  KC_U,  KC_Y, KC_QUOT, KC_BSPC,
-  //|------+------+------+------+------+------|                |------+------+-------+------+-------+--------|
-    KC_LCTL,  KC_A,  KC_R,  KC_S,  KC_T,  KC_G,                   KC_M,  KC_N,  KC_E,  KC_I, KC_O, KC_SCLN,
-  //|------+------+------+------+------+------|                |------+------+-------+------+-------+--------|
-    KC_LSPO,  KC_Z,  KC_X,  KC_C,  KC_D,  KC_V,                   KC_K,  KC_H,KC_COMM,KC_DOT,KC_SLSH,KC_RSPC,
-  //|------+------+------+------+------+------+------|  |------+------+------+-------+------+-------+--------|
-                               KC_LGESC,LOWER, KC_SPC,   RCTL_T(KC_ENT), RAISE, KC_RACL
-                              //`--------------------'  `--------------------'
-  ),
-
-  [_LOWER] = LAYOUT(
-  //,---------------------------------------------.                ,-----------------------------------------.
-     KC_ESC,  KC_1, KC_2,   KC_3,   KC_4,   KC_5,                    KC_6,  KC_7,  KC_8,  KC_9,  KC_0, KC_DEL,
-  //|------+------+-------+-------+-------+-------|                |------+------+------+------+------+------|
-    KC_LCTL, KC_NO,KC_MS_L,KC_MS_D,KC_MS_U,KC_MS_R,                KC_LEFT,KC_DOWN,KC_UP,KC_RIGHT,KC_NO,KC_NO,
-  //|------+------+-------+-------+-------+-------|                |------+------+------+------+------+------|
-    KC_LSFT, KC_NO,KC_BTN2,KC_WH_D,KC_WH_U,KC_BTN1,                KC_HOME,KC_PGDN,KC_PGUP,KC_END,KC_NO,KC_NO,
-  //|------+------+-------+-------+-------+-------+------|  |------+------+------+------+------+------+------|
-                                    KC_LGUI, LOWER,KC_SPC,   KC_ENT, RAISE,KC_RALT
-                                  //`--------------------'  `--------------------'
-  ),
-
-  [_RAISE] = LAYOUT(
-  //,-----------------------------------------.                ,-----------------------------------------.
-     KC_ESC,KC_EXLM,KC_AT,KC_HASH,KC_DLR,KC_PERC,              KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_BSPC,
-  //|------+------+------+------+------+------|                |------+------+------+------+------+------|
-    KC_LCTL, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,                KC_MINS,KC_EQL,KC_LCBR,KC_RCBR,KC_PIPE,KC_GRV,
-  //|------+------+------+------+------+------|                |------+------+------+------+------+------|
-    KC_LSFT, KC_F6, KC_F7, KC_F8, KC_F9,KC_F10,                KC_UNDS,KC_PLUS,KC_LBRC,KC_RBRC,KC_BSLS,KC_TILD,
-  //|------+------+------+------+------+------+------|  |------+------+------+------+------+------+------|
-                                KC_LGUI, LOWER,KC_SPC,   KC_ENT, RAISE,KC_RALT
-                              //`--------------------'  `--------------------'
-  ),
-
-  [_ADJUST] = LAYOUT(
-  //,-----------------------------------------.                ,-----------------------------------------.
-      RESET,RGBRST, KC_NO, KC_NO, KC_NO, KC_NO,                  KC_NO,KC__MUTE, KC_NO, KC_NO, KC_NO, KC_NO,
-  //|------+------+------+------+------+------|                |------+------+------+------+------+------|
-    RGB_TOG,RGB_HUI,RGB_SAI,RGB_VAI,RGB_SPI,KC_NO,               KC_PAUSE,KC__VOLUP, KC_NO, KC_NO, KC_NO, KC_NO,
-  //|------+------+------+------+------+------|                |------+------+------+------+------+------|
-    RGB_MOD,RGB_HUD,RGB_SAD,RGB_VAD,RGB_SPD,KC_NO,               KC_SCROLLLOCK,KC__VOLDOWN, KC_NO, KC_NO, KC_NO, RGB_RMOD,
-  //|------+------+------+------+------+------+------|  |------+------+------+------+------+------+------|
-                                KC_LGUI, LOWER,KC_SPC,   KC_ENT, RAISE,KC_RALT
-                              //`--------------------'  `--------------------'
-  )
+	[0] = LAYOUT_split_3x6_3(KC_DEL, KC_Q, LGUI_T(KC_W), LCTL_T(KC_F), KC_P, KC_B, KC_J, KC_L, RCTL_T(KC_U), RGUI_T(KC_Y), KC_QUOT, KC_RSFT, KC_BSPC, KC_A, LALT_T(KC_R), LCTL_T(KC_S), LSFT_T(KC_T), KC_G, KC_M, RSFT_T(KC_N), RCTL_T(KC_E), RALT_T(KC_I), KC_O, KC_SCLN, KC_CAPS, KC_Z, KC_X, KC_C, KC_D, KC_V, KC_K, KC_H, KC_COMM, KC_DOT, KC_SLSH, KC_ESC, NAV_CKC, NUM_CKC, FUNC_TAB_CKC, KC_BSPC, KC_SPC, KC_TAB),
+	[1] = LAYOUT_split_3x6_3(KC_TRNS, KC_NO, KC_LGUI, LCTL(KC_F), KC_NO, KC_NO, KC_NO, KC_7, KC_8, KC_9, KC_NO, KC_NO, KC_TRNS, KC_LSFT, KC_LALT, KC_LCTL, SYM_CKC, KC_NO, KC_NO, KC_4, KC_5, KC_6, KC_0, KC_NO, KC_TRNS, LCTL(KC_Z), LCTL(KC_X), LCTL(KC_C), LCTL(KC_D), LCTL(KC_V), KC_NO, KC_1, KC_2, KC_3, KC_MINS, KC_NO, KC_NO, KC_TRNS, KC_NO, KC_DEL, KC_ENT, KC_TAB),
+	[2] = LAYOUT_split_3x6_3(KC_TRNS, KC_NO, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_LCBR, KC_RCBR, KC_GRV, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_NO, KC_LPRN, KC_RPRN, KC_SCLN, KC_EQL, KC_TRNS, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_LBRC, KC_RBRC, KC_BSLS, KC_MINS, KC_TRNS, KC_NO, KC_TRNS, KC_NO, KC_DEL, KC_ENT, KC_TAB),
+	[3] = LAYOUT_split_3x6_3(KC_TRNS, KC_NO, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_INS, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, MOUSE_CKC, KC_NO, KC_HOME, KC_PGDN, KC_PGUP, KC_END, KC_NO, KC_TRNS, LCTL(KC_Z), LCTL(KC_X), LCTL(KC_C), LCTL(KC_D), LCTL(KC_V), KC_NO, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO, KC_TRNS, KC_NO, KC_NO, KC_DEL, KC_ENT, KC_TAB),
+	[4] = LAYOUT_split_3x6_3(KC_TRNS, KC_NO, KC_LGUI, C_S_F, KC_NO, KC_NO, KC_NO, KC_F7, KC_F8, KC_F9, KC_F11, KC_TRNS, KC_TRNS, KC_ESC, KC_LALT, KC_LCTL, KC_LSFT, KC_NO, KC_NO, KC_F4, KC_F5, KC_F6, KC_F10, KC_TRNS, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, C_S_V, KC_NO, KC_F1, KC_F2, KC_F3, KC_F12, KC_TRNS, KC_NO, KC_NO, KC_TRNS, KC_DEL, KC_ENT, KC_TAB),
+	[5] = LAYOUT_split_3x6_3(BL_TOGG, KC_MPRV, KC_LGUI, KC_MPLY, KC_MNXT, KC_NO, KC_NO, KC_MUTE, KC_VOLD, KC_VOLU, KC_NO, RESET, RGB_TOG, KC_LSFT, KC_LALT, KC_LCTL, KC_RSFT, KC_TRNS, KC_NO, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, KC_NO, KC_NO, KC_NO, KC_ACL0, KC_ACL1, KC_ACL2, KC_NO, KC_NO, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_NO, KC_NO, BASE_CKC, KC_NO, KC_BTN2, KC_BTN1, KC_BTN3)
 };
 
 int RGB_current_mode;
 
 // Setting ADJUST layer RGB back to default
-void update_tri_layer_RGB(uint8_t layer1, uint8_t layer2, uint8_t layer3) {
-  if (IS_LAYER_ON(layer1) && IS_LAYER_ON(layer2)) {
-    layer_on(layer3);
-  } else {
-    layer_off(layer3);
-  }
-}
+// void update_tri_layer_RGB(uint8_t layer1, uint8_t layer2, uint8_t layer3) {
+//   if (IS_LAYER_ON(layer1) && IS_LAYER_ON(layer2)) {
+//     layer_on(layer3);
+//   } else {
+//     layer_off(layer3);
+//   }
+// }
 
 void matrix_init_user(void) {
     #ifdef RGBLIGHT_ENABLE
@@ -257,27 +227,37 @@ void render_layer_state(void) {
         0x20, 0x94, 0x95, 0x96, 0x20,
         0x20, 0xb4, 0xb5, 0xb6, 0x20,
         0x20, 0xd4, 0xd5, 0xd6, 0x20, 0};
-    static const char PROGMEM raise_layer[] = {
-        0x20, 0x97, 0x98, 0x99, 0x20,
-        0x20, 0xb7, 0xb8, 0xb9, 0x20,
-        0x20, 0xd7, 0xd8, 0xd9, 0x20, 0};
-    static const char PROGMEM lower_layer[] = {
-        0x20, 0x9a, 0x9b, 0x9c, 0x20,
-        0x20, 0xba, 0xbb, 0xbc, 0x20,
-        0x20, 0xda, 0xdb, 0xdc, 0x20, 0};
+    // static const char PROGMEM raise_layer[] = {
+    //     0x20, 0x97, 0x98, 0x99, 0x20,
+    //     0x20, 0xb7, 0xb8, 0xb9, 0x20,
+    //     0x20, 0xd7, 0xd8, 0xd9, 0x20, 0};
+    // static const char PROGMEM lower_layer[] = {
+    //     0x20, 0x9a, 0x9b, 0x9c, 0x20,
+    //     0x20, 0xba, 0xbb, 0xbc, 0x20,
+    //     0x20, 0xda, 0xdb, 0xdc, 0x20, 0};
     static const char PROGMEM adjust_layer[] = {
         0x20, 0x9d, 0x9e, 0x9f, 0x20,
         0x20, 0xbd, 0xbe, 0xbf, 0x20,
         0x20, 0xdd, 0xde, 0xdf, 0x20, 0};
-    if(layer_state_is(_ADJUST)) {
+
+    uint8_t layer = get_highest_layer(layer_state);
+
+    if(layer > _BASE) {
         oled_write_P(adjust_layer, false);
-    } else if(layer_state_is(_LOWER)) {
-        oled_write_P(lower_layer, false);
-    } else if(layer_state_is(_RAISE)) {
-        oled_write_P(raise_layer, false);
     } else {
         oled_write_P(default_layer, false);
     }
+
+
+    // if(layer_state_is(_ADJUST)) {
+    //     oled_write_P(adjust_layer, false);
+    // } else if(layer_state_is(_LOWER)) {
+    //     oled_write_P(lower_layer, false);
+    // } else if(layer_state_is(_RAISE)) {
+    //     oled_write_P(raise_layer, false);
+    // } else {
+    //     oled_write_P(default_layer, false);
+    // }
 }
 
 void render_status_main(void) {
@@ -315,6 +295,7 @@ void oled_task_user(void) {
 }
 
 #endif
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   if (record->event.pressed) {
 #ifdef OLED_DRIVER_ENABLE
@@ -322,72 +303,72 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 #endif
     // set_timelog();
   }
-  static uint16_t my_colon_timer;
+//   static uint16_t my_colon_timer;
 
-  switch (keycode) {
-    case LOWER:
-      if (record->event.pressed) {
-        layer_on(_LOWER);
-        update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
-      } else {
-        layer_off(_LOWER);
-        update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
-      }
-      return false;
-    case RAISE:
-      if (record->event.pressed) {
-        layer_on(_RAISE);
-        update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
-      } else {
-        layer_off(_RAISE);
-        update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
-      }
-      return false;
-    case ADJUST:
-        if (record->event.pressed) {
-          layer_on(_ADJUST);
-        } else {
-          layer_off(_ADJUST);
-        }
-        return false;
-    case KC_RACL:
-        if (record->event.pressed) {
-          my_colon_timer = timer_read();
-          register_code(KC_RALT);
-        } else {
-          unregister_code(KC_RALT);
-          if (timer_elapsed(my_colon_timer) < TAPPING_TERM) {
-            SEND_STRING(":"); // Change the character(s) to be sent on tap here
-          }
-        }
-        return false;
-    case RGBRST:
-      #ifdef RGBLIGHT_ENABLE
-        if (record->event.pressed) {
-          eeconfig_update_rgblight_default();
-          rgblight_enable();
-          RGB_current_mode = rgblight_config.mode;
-        }
-      #endif
-      #ifdef RGB_MATRIX_ENABLE
-        if (record->event.pressed) {
-          eeconfig_update_rgb_matrix_default();
-          rgb_matrix_enable();
-        }
-      #endif
-      break;
-  }
+//   switch (keycode) {
+//     case LOWER:
+//       if (record->event.pressed) {
+//         layer_on(_LOWER);
+//         update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
+//       } else {
+//         layer_off(_LOWER);
+//         update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
+//       }
+//       return false;
+//     case RAISE:
+//       if (record->event.pressed) {
+//         layer_on(_RAISE);
+//         update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
+//       } else {
+//         layer_off(_RAISE);
+//         update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
+//       }
+//       return false;
+//     case ADJUST:
+//         if (record->event.pressed) {
+//           layer_on(_ADJUST);
+//         } else {
+//           layer_off(_ADJUST);
+//         }
+//         return false;
+//     case KC_RACL:
+//         if (record->event.pressed) {
+//           my_colon_timer = timer_read();
+//           register_code(KC_RALT);
+//         } else {
+//           unregister_code(KC_RALT);
+//           if (timer_elapsed(my_colon_timer) < TAPPING_TERM) {
+//             SEND_STRING(":"); // Change the character(s) to be sent on tap here
+//           }
+//         }
+//         return false;
+//     case RGBRST:
+//       #ifdef RGBLIGHT_ENABLE
+//         if (record->event.pressed) {
+//           eeconfig_update_rgblight_default();
+//           rgblight_enable();
+//           RGB_current_mode = rgblight_config.mode;
+//         }
+//       #endif
+//       #ifdef RGB_MATRIX_ENABLE
+//         if (record->event.pressed) {
+//           eeconfig_update_rgb_matrix_default();
+//           rgb_matrix_enable();
+//         }
+//       #endif
+//       break;
+//   }
   return true;
 }
 
-#ifdef RGB_MATRIX_ENABLE
+// #ifdef RGB_MATRIX_ENABLE
 
-void suspend_power_down_keymap(void) {
-    rgb_matrix_set_suspend_state(true);
-}
+// void suspend_power_down_keymap(void) {
+//     rgb_matrix_set_suspend_state(true);
+// }
 
-void suspend_wakeup_init_keymap(void) {
-    rgb_matrix_set_suspend_state(false);
-}
+// void suspend_wakeup_init_keymap(void) {
+//     rgb_matrix_set_suspend_state(false);
+// }
 
-#endif
+// #endif
